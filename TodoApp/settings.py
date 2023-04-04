3e49c928c79e1a6196abb0bf3e63944be053e3fd
FELONYANGO <felixonyango951@gmail.com>
2023-04-04 21:58:12 +0300

    static errror

--- TodoApp/settings.py
+++ TodoApp/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 import environ
 
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'routine',
+    
 ]
 
 MIDDLEWARE = [
@@ -123,5 +124,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS=[BASE_DIR /'static']
+STATIC_URL = 'static/'
+STATICFILES_DIRS =[os.path.join(BASE_DIR / 'static'),]
